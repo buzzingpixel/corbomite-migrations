f8f7ae2a3fce76be608949fadef683308be87753
TJ Draper <tj@buzzingpixel.com>
2019-01-05 16:42:34 -0600

    Add copyright banners to PHP files

--- src/actionConfig.php
+++ src/actionConfig.php
@@ -1,6 +1,12 @@
 <?php
 declare(strict_types=1);
 
+/**
+ * @author TJ Draper <tj@buzzingpixel.com>
+ * @copyright 2019 BuzzingPixel, LLC
+ * @license Apache-2.0
+ */
+
 use corbomite\migrations\actions\MigrateUpAction;
 use corbomite\migrations\actions\MigrateDownAction;
 use corbomite\migrations\actions\CreateSampleConfig;
