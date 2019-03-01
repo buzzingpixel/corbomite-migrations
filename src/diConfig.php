a8aaefc0914712e465bcf643a6d510811d9293b7
TJ Draper <tj@buzzingpixel.com>
2019-02-28 23:28:55 -0600

    Update DI config to rely on ContainerInterface rather than Corbomite DI

--- src/diConfig.php
+++ src/diConfig.php
@@ -7,10 +7,10 @@ declare(strict_types=1);
  * @license Apache-2.0
  */
 
-use corbomite\di\Di;
 use corbomite\cli\ExitStatement;
 use Composer\Autoload\ClassLoader;
 use Phinx\Console\PhinxApplication;
+use Psr\Container\ContainerInterface;
 use corbomite\cli\factories\ArrayInputFactory;
 use corbomite\cli\services\CliQuestionService;
 use corbomite\migrations\actions\MigrateUpAction;
@@ -23,7 +23,7 @@ use corbomite\migrations\actions\CreateMigrationAction;
 use corbomite\migrations\utilities\CaseConversionUtility;
 
 return [
-    PreFlightService::class => function () {
+    PreFlightService::class => static function () {
         if (defined('APP_BASE_PATH')) {
             $appBasePath = APP_BASE_PATH;
         } else {
@@ -38,7 +38,7 @@ return [
             new ConsoleOutput()
         );
     },
-    CreateSampleConfig::class => function () {
+    CreateSampleConfig::class => static function () {
         if (defined('APP_BASE_PATH')) {
             $appBasePath = APP_BASE_PATH;
         } else {
@@ -53,39 +53,39 @@ return [
             __DIR__
         );
     },
-    CreateMigrationAction::class => function () {
+    CreateMigrationAction::class => static function (ContainerInterface $di) {
         return new CreateMigrationAction(
-            Di::get(PreFlightService::class),
-            Di::get(CliQuestionService::class),
+            $di->get(PreFlightService::class),
+            $di->get(CliQuestionService::class),
             new CaseConversionUtility(),
             new PhinxApplication(),
             new ArrayInputFactory(),
             new ConsoleOutput()
         );
     },
-    MigrateStatusAction::class => function () {
+    MigrateStatusAction::class => function (ContainerInterface $di) {
         return new MigrateStatusAction(
-            Di::get(PreFlightService::class),
+            $di->get(PreFlightService::class),
             new PhinxApplication(),
             new ArrayInputFactory(),
             new ConsoleOutput()
         );
     },
-    MigrateUpAction::class => function () {
+    MigrateUpAction::class => function (ContainerInterface $di) {
         return new MigrateUpAction(
-            Di::get(PreFlightService::class),
+            $di->get(PreFlightService::class),
             new PhinxApplication(),
             new ArrayInputFactory(),
             new ConsoleOutput()
         );
     },
-    MigrateDownAction::class => function () {
+    MigrateDownAction::class => function (ContainerInterface $di) {
         return new MigrateDownAction(
-            Di::get(PreFlightService::class),
+            $di->get(PreFlightService::class),
             new PhinxApplication(),
             new ArrayInputFactory(),
             new ConsoleOutput(),
-            Di::get(CliQuestionService::class)
+            $di->get(CliQuestionService::class)
         );
     },
 ];
